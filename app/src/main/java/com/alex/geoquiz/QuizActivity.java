8a8c9c928d1e91d89fb534ee91b17d0d74e5d045
Alexander Slatinin <sandroisu1411@gmail.com>
2019-11-12 21:05:04 +0300

    Maybe good enought

--- app/src/main/java/com/alex/geoquiz/QuizActivity.java
+++ app/src/main/java/com/alex/geoquiz/QuizActivity.java
@@ -3,26 +3,18 @@ package com.alex.geoquiz;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-import static android.view.Gravity.BOTTOM;
-import static android.view.Gravity.CENTER_VERTICAL;
-import static android.view.Gravity.TOP;
 import static android.widget.Toast.makeText;
 
 public class QuizActivity extends AppCompatActivity {
-    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
@@ -30,7 +22,9 @@ public class QuizActivity extends AppCompatActivity {
     private TextView mQuestionTextView;
     private static final String KEY_INDEX = "index";
     private ArrayList<Integer> mArrayList;
+    private int questionQuantity = 0;
     private float counter = 0;
+    boolean isItTimeToStart = false;
     private Question[] mQuestionBank = {
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
@@ -50,13 +44,22 @@ public class QuizActivity extends AppCompatActivity {
         mNextButton = findViewById(R.id.next_button);
         mQuestionTextView = findViewById(R.id.question_text_view);
         mPreviousButton = findViewById(R.id.previous_button);
-        mArrayList = new ArrayList<>(mQuestionBank.length);
-        setZero();
-        newGame();
+        questionQuantity = mQuestionBank.length;
+        mArrayList = new ArrayList<>(questionQuantity);
+        for (int i = 0; i < questionQuantity; i++) {
+            mArrayList.add(i, 0);
+        }
+
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
-        Log.d(TAG, "onCreate(Bundle) called");
+
+        updateQuestion();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,9 +75,12 @@ public class QuizActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mPreviousButton.setEnabled(true);
+                mPreviousButton.setVisibility(View.VISIBLE);
                 increaseIndex();
                 updateQuestion();
+                if (mCurrentIndex==questionQuantity-1)
+                    isItTimeToStart = true;
+
             }
         });
         mPreviousButton.setOnClickListener(new View.OnClickListener() {
@@ -84,34 +90,30 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-        updateQuestion();
     }
 
     private void increaseIndex() {
-        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-        if ((mCurrentIndex + 1) % mQuestionBank.length == 0) {
-            newGame();
-        }
+        mCurrentIndex = (mCurrentIndex + 1) % questionQuantity;
+
     }
 
     private void decreaseIndex() {
-        if (mCurrentIndex == 0)
-            mPreviousButton.setEnabled(false);
-        else
             mCurrentIndex = (mCurrentIndex - 1);
     }
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+        if (mCurrentIndex==0) {
+            mPreviousButton.setVisibility(View.GONE);
+            if(isItTimeToStart) {
+                newGame();
+                counter = 0;
+            }
+        }
         if (mArrayList.get(mCurrentIndex) == 1) {
             buttonEnable(false);
         } else buttonEnable(true);
-        if(counter!=0&&mCurrentIndex==0) {
-            counter = counter * 100 / mQuestionBank.length;
-            Toast.makeText(QuizActivity.this, "Ваш результат: " + (int)counter + "%", Toast.LENGTH_SHORT).show();
-            counter = 0;
-        }
     }
 
     private void checkAnswer(boolean userPressedTrue) {
@@ -120,40 +122,12 @@ public class QuizActivity extends AppCompatActivity {
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
             counter++;
-        } else messageResId = R.string.incorrect_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
         makeText(this, messageResId, Toast.LENGTH_LONG).show();
         buttonEnable(false);
         mArrayList.set(mCurrentIndex, 1);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
@@ -162,20 +136,14 @@ public class QuizActivity extends AppCompatActivity {
         outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
-    private void setZero() {
-        for (int i = 0; i < mQuestionBank.length; i++) {
-            mArrayList.add(i, 0);
-        }
-    }
-
     private void newGame() {
-        mPreviousButton.setEnabled(false);
-        for (int i = 0; i < mQuestionBank.length; i++) {
-            mArrayList.set(i, 0);
-        }
-
-
+        for (int i = 0; i < questionQuantity; i++) {
+            mArrayList.set(i, 0);}
+            counter = counter * 100 / questionQuantity;
+            Toast.makeText(QuizActivity.this, "Ваш результат: " + (int) counter + "%", Toast.LENGTH_SHORT).show();
+            isItTimeToStart =false;
     }
+
 
     private void buttonEnable(boolean b) {
         mTrueButton.setEnabled(b);
